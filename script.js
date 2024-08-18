7d5697632e07be9f6176b416b9f9dbea24673924
ybaggio <Ybaggio2010@outlook.com>
2024-08-18 10:40:10 +0800

    更新 script.js

--- script.js
+++ script.js
@@ -4,27 +4,18 @@ function change() {
   if (i == 3) i = 0;
   if (i == 0) {
     document.getElementById("xiezhen3").style.display = "none";
-    document.getElementById("xiezhen3").style.opacity = 0;
     document.getElementById("xiezhen1").style.display = "flex";
-    document.getElementById("xiezhen1").style.opacity = 1;
   } else if (i == 1) {
     document.getElementById("xiezhen1").style.display = "none";
-    document.getElementById("xiezhen1").style.opacity = 0;
     document.getElementById("xiezhen2").style.display = "flex";
-    document.getElementById("xiezhen2").style.opacity = 1;
   } else if (i == 2) {
     document.getElementById("xiezhen2").style.display = "none";
-    document.getElementById("xiezhen2").style.opacity = 0;
     document.getElementById("xiezhen3").style.display = "flex";
-    document.getElementById("xiezhen3").style.opacity = 1;
   }
 }
 function main() {
   document.getElementById("xiezhen1").style.opacity = "flex";
   document.getElementById("xiezhen2").style.display = "none";
   document.getElementById("xiezhen3").style.display = "none";
-  document.getElementById("xiezhen1").style.opacity = 1;
-  document.getElementById("xiezhen2").style.opacity = 0;
-  document.getElementById("xiezhen3").style.opacity = 0;
   setInterval(change, 5000);
 }
